3955382b204e4a2720513e6e42f23339957b5b38
rajatpatra12 <rajatpatra12@gmail.com>
2018-09-14 13:11:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var recipeName = "Creamy squash linguine";
+var preparationTime = 45;
+var recipeBy = "Jimmy Cutter";
+var isVegetarian = true;
+var calories = 223.7;
+
+console.log("Recipe Details");
+console.log("-----------------");
+console.log(recipeName);
+console.log(preparationTime);
+console.log(recipeBy);
+console.log(isVegetarian);
+console.log(calories);
+
+var preparationTime = 60;
+var recipeBy = "Jimmy Cutter George";
+
+console.log("Recipe Details after update");
+console.log("-----------------");
+console.log(recipeName);
+console.log(preparationTime);
+console.log(recipeBy);
+console.log(isVegetarian);
+console.log(calories);
